f0810726c25334722d731720768ccbdb1f50dc6c
Taisuke Fukuno <fukuno@jig.jp>
2024-03-08 16:07:55 +0900

    change genre to folder

--- generate_files_json.js
+++ generate_files_json.js
@@ -2,12 +2,11 @@ import { CSV } from "https://js.sabae.cc/CSV.js";
 import { ArrayUtil } from "https://js.sabae.cc/ArrayUtil.js";
 
 const data = await CSV.fetchJSON("./files.csv");
-console.log(data);
-const genres = ArrayUtil.toUnique(data.map(i => i.genre));
-const json = { videos: genres.map(i => ({ name: i, list: data.filter(j => j.genre == i).map(j => {
+const folders = ArrayUtil.toUnique(data.map(i => i.folder));
+const json = { videos: folders.map(i => ({ name: i, list: data.filter(j => j.folder == i).map(j => {
   const o = {};
   Object.assign(o, j);
-  delete o.genre;
+  delete o.folder;
   return o;
 })}))}
-await Deno.writeTextFile("files2.json", JSON.stringify(json, null, 2));
+await Deno.writeTextFile("files.json", JSON.stringify(json, null, 2));
